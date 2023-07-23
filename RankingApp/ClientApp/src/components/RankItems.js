76c41b12bfff3668d65d37a57838f7e328fd4929
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-07-24 01:18:07 +0700

    finished

--- RankingApp/ClientApp/src/components/RankItems.js
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -1,33 +1,78 @@
-﻿import React, { useState, useEffect } from 'react';
+﻿import { useEffect, useState } from 'react';
+import Item from './Item.js';
+import ItemCollection from './ItemCollection.js';
 import MovieImageArr from './MovieImages.js';
+import RankingGrid from "./RankingGrid";
 
+const RankItems = ({ items, setItems, dataType, imgArr, localStorageKey }) => {
 
-const RankItems = () => {
+    const [reload, setReload] = useState(false);
 
-    const [items, setItems] = useState([]);
-    const dataType = 1;
+    function Reload() {
+        setReload(true);
+    }
+
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    function drop(ev) {
+
+        ev.preventDefault();
+        const targetElm = ev.target;
+        if (targetElm.nodeName === "IMG") {
+            return false;
+        }
+        if (targetElm.childNodes.length === 0) {
+            var data = parseInt(ev.dataTransfer.getData("text").substring(5));
+            const transformedCollection = items.map((item) => (item.id === parseInt(data)) ?
+                { ...item, ranking: parseInt(targetElm.id.substring(5)) } : { ...item, ranking: item.ranking });
+            setItems(transformedCollection);
+        }
+
+    }
 
     useEffect(() => {
+        if (items == null) {
+            getDataFromApi();
+        }
+    }, [dataType])
+
+    function getDataFromApi() {
         fetch(`item/${dataType}`)
             .then((results) => {
                 return results.json();
             })
             .then(data => {
-                setItems(data); 
+                setItems(data);
             })
-    }, [])
+    }
 
+    useEffect(() => {
+        if (items != null) {
+            localStorage.setItem(localStorageKey, JSON.stringify(items));
+        }
+        setReload(false);
+    }, [items])
+
+    useEffect(() => {
+        if (reload === true) {
+            getDataFromApi();
+        }
+    },[reload])
+        
     return (
+        (items != null)?
         <main>
-            <div className="items-not-ranked">
-            {
-                    (items.length > 0) ? items.map((item) =>
-                        <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image } />
-
-                    ) : <div>Loading...</div>
-            }
-            </div>
+            <RankingGrid items={items} imgArr={imgArr} drag={drag} allowDrop={allowDrop} drop={drop }/>
+            <ItemCollection items={items} drag={drag} imgArr={imgArr} />
+            <button onClick={Reload} className ="reload" style={{"margin-Top":"10px"}}> <span class="text">Reload</span></button>
         </main>
+        : <main>Loading...</main>
     )
 }
 export default RankItems;
